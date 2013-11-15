651964cb7039075c03440d158452f57e195f2f0e
James Dabbs <jamesdabbs@gmail.com>
2013-11-15 05:23:59 +0000

    Add scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,6 @@ curl https://raw.github.com/jamesdabbs/.rc/master/ssh-keys.py -o ssh-key.py
 python ssh-key.py
 git clone git@github.com:jamesdabbs/.rc.git
 
+.rc/scripts/oh-my-zsh.sh
+.rc/scripts/rbenv.sh
+
