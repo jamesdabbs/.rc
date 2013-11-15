c2beab4e35d478cd905305325b9f116221066428
James Dabbs <jamesdabbs@gmail.com>
2013-11-15 15:23:44 +0000

    Fix tmux conf path

--- scripts/link-templates.sh
+++ scripts/link-templates.sh
@@ -4,7 +4,7 @@ ln -s ~/.rc/templates/gemrc ~/.gemrc
 ln -s ~/.rc/templates/gitconfig ~/.gitconfig
 ln -s ~/.rc/templates/gitignore_global ~/.gitignore_global
 ln -s ~/.rc/templates/rspec ~/.rspec
-ln -s ~/.rc/templates/tmux.conf ~/tmux.conf
+ln -s ~/.rc/templates/tmux.conf ~/.tmux.conf
 ln -s ~/.rc/templates/vimrc ~/.vimrc
 ln -s ~/.rc/templates/zsh_aliases ~/.zsh_aliases
 
