36147ccbd0f75e3ed2ad2aebe8f4084964b6e40b
James Dabbs <jamesdabbs@gmail.com>
2013-11-15 07:41:56 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 curl https://raw.github.com/jamesdabbs/.rc/master/ssh-keys.py -o ssh-key.py
+mkdir -p $HOME/.ssh
 python ssh-key.py
-git clone git@github.com:jamesdabbs/.rc.git
-.rc/setup.sh
+git clone git@github.com:jamesdabbs/.rc.git $HOME/.rc
+$HOME/.rc/setup.sh
 
