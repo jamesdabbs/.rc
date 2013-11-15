560354f854d73d441ba6199710ed60005aa9e5e8
James Dabbs <jamesdabbs@gmail.com>
2013-11-15 12:49:05 +0000

    Slightly more descriptive messages

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 #!/bin/zsh
 
+echo -n "\n== OH MY ZSH =="
 ~/.rc/scripts/oh-my-zsh.sh
+
+echo -n "\n== RBENV =="
 ~/.rc/scripts/rbenv.sh
+
+echo -n "\n== VIM PLUGINS =="
 ~/.rc/scripts/vim.sh
+
+echo -n "\n== LINKING CONFIG FILES =="
 ~/.rc/scripts/link-templates.sh
 
-source ~/.zshrc
+echo -n "\nDone. You should 'source ~/.zshrc' to get started"
 
