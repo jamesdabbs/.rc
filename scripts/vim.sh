0fcf22e577d2af59b11242b9c1c29cbea9bbf528
James Dabbs <jamesdabbs@gmail.com>
2013-11-20 10:51:18 -0500

    Use vundle

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
 
-# Pathogen
-if [[ ! -f ~/.vim/autoload/pathogen.vim ]]; then
-  echo "  --> Installing Pathogen"
-  mkdir -p ~/.vim/autoload ~/.vim/bundle
-  curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+if [[ ! -d ~/.vim/bundle/vundle ]]; then
+  echo "Installing vundle"
+  mkdir -p ~/.vim/bundle
+  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 fi
-
-# Plugins
-plugins=( mileszs/ack.vim kien/ctrlp.vim scrooloose/nerdtree airblade/vim-gitgutter tpope/vim-fugitive )
-for repo in "${plugins[@]}"
-do
-  name=${repo#*/}
-  path=$HOME/.vim/bundle/${name}
-
-  echo -n "$repo --> "
-  if [[ -d $path ]]; then
-    cd $path && git pull >/dev/null
-    echo "Updated"
-  else
-    echo "Cloning"
-    cd $HOME/.vim/bundle && git clone git://github.com/$repo >/dev/null
-  fi
-done
+vim +BundleInstall +qall
 
