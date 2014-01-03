84481937952ac8e2a241aa1be2e5e7f985135a01
James Dabbs <jamesdabbs@gmail.com>
2014-01-03 10:58:27 -0500

    Add libsqlite3-dev package

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -2,5 +2,5 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
-sudo apt-get install -y ack-grep build-essential curl git man sudo tree tmux vim zsh
+sudo apt-get install -y ack-grep build-essential curl git libsqlite3-dev man sudo tree tmux vim zsh
 
