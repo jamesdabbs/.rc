7a6b605b6a869c61df6b20ac46a31609a4e941c4
James Dabbs <jamesdabbs@gmail.com>
2014-01-29 16:33:39 +0000

    Skip install confirmation

--- scripts/rbenv.sh
+++ scripts/rbenv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install build-essential autoconf libssl-dev libyaml-dev libreadline6 libreadline6-dev zlib1g zlib1g-dev
+sudo apt-get install -y build-essential autoconf libssl-dev libyaml-dev libreadline6 libreadline6-dev zlib1g zlib1g-dev
 
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
